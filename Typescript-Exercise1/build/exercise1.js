fc670d0716a691ec7dc585a9d508f766ccf18a7b
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2025-05-29 14:57:48 +0200

    Done with Exercise1

--- Typescript-Exercise1/build/exercise1.js
+++ Typescript-Exercise1/build/exercise1.js
@@ -1,2 +1,61 @@
 "use strict";
-console.log("Hristijan Ralevski");
+// TYPESCRIPT 
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+// FETCHING DATA
+function throwError(e) {
+    throw new Error("Error!");
+}
+const albumsContainer = document.getElementById("albums");
+const commentsContainer = document.getElementById("comments");
+try {
+    const fetchAlbums = () => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield fetch("https://jsonplaceholder.typicode.com/albums");
+        const data = yield response.json();
+        return data;
+    });
+    const fetchComments = (id) => __awaiter(void 0, void 0, void 0, function* () {
+        const response = yield fetch("https://jsonplaceholder.typicode.com/albums/${id}/comments");
+        const data = yield response.json();
+        return data;
+    });
+    // FUNCTION FOR DISPLAY COMMENTS
+    const displayComments = (id) => __awaiter(void 0, void 0, void 0, function* () {
+        const comments = yield fetchComments(id);
+        albumsContainer === null || albumsContainer === void 0 ? void 0 : albumsContainer.classList.add("hide");
+        albumsContainer === null || albumsContainer === void 0 ? void 0 : albumsContainer.classList.remove("show");
+        commentsContainer === null || commentsContainer === void 0 ? void 0 : commentsContainer.classList.remove("hide");
+        commentsContainer === null || commentsContainer === void 0 ? void 0 : commentsContainer.classList.add("show");
+        comments.forEach((comment) => {
+            const commentItem = document.createElement("div");
+            commentItem.classList.add("comment-item");
+            commentItem.innerHTML = `<p>${comment.body}</p>`;
+            commentsContainer === null || commentsContainer === void 0 ? void 0 : commentsContainer.appendChild(commentItem);
+        });
+    });
+    // FUNCTION FOR DISPLAY ALBUM-ITEM
+    const displayAlbums = () => __awaiter(void 0, void 0, void 0, function* () {
+        const albums = yield fetchAlbums();
+        albums.forEach((album) => {
+            const albumItem = document.createElement("div");
+            albumItem.classList.add("album-item");
+            albumItem.innerHTML = `<p>${album.title}</p>`;
+            albumsContainer === null || albumsContainer === void 0 ? void 0 : albumsContainer.appendChild(albumItem);
+            // Add eventListener on albumItem
+            albumItem.addEventListener('click', (e) => {
+                displayComments(album.id);
+            });
+        });
+    });
+    displayAlbums();
+}
+catch (e) {
+    throwError(e);
+}
--- Typescript-Exercise1/src/exercise1.ts
+++ Typescript-Exercise1/src/exercise1.ts
@@ -1,1 +1,84 @@
-console.log("Hristijan Ralevski")
+// TYPESCRIPT 
+
+// CREATING INTERFACES
+
+interface Album{
+    userID: number;
+    id: number;
+    title: string;
+}
+
+interface Comment{
+    postID: number;
+    id: number;
+    name: string;
+    body: string;
+}
+
+
+// FETCHING DATA
+
+function throwError(e: unknown): never{
+    throw new Error("Error!");
+}
+
+const albumsContainer = document.getElementById("albums");
+const commentsContainer = document.getElementById("comments");
+
+try{
+    const fetchAlbums = async () => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+        const data: Album[] = await response.json();
+        return data;
+
+    }
+
+     const fetchComments = async (id: number) => {
+        const response = await fetch("https://jsonplaceholder.typicode.com/albums/${id}/comments");
+        const data: Comment[] = await response.json();
+        return data;
+
+    }
+
+    // FUNCTION FOR DISPLAY COMMENTS
+    const displayComments = async (id: number) =>{
+
+        const comments = await fetchComments(id);
+      albumsContainer?.classList.add("hide");     
+      albumsContainer?.classList.remove("show");  
+
+        commentsContainer?.classList.remove("hide"); 
+        commentsContainer?.classList.add("show"); 
+      
+
+          comments.forEach((comment) => {
+            const commentItem = document.createElement("div");
+            commentItem.classList.add("comment-item")
+            commentItem.innerHTML = `<p>${comment.body}</p>`;
+            commentsContainer?.appendChild(commentItem);
+
+          
+        })
+        
+    }
+
+    // FUNCTION FOR DISPLAY ALBUM-ITEM
+    const displayAlbums = async () =>{
+        const albums = await fetchAlbums();
+        albums.forEach((album) => {
+            const albumItem = document.createElement("div");
+            albumItem.classList.add("album-item")
+            albumItem.innerHTML = `<p>${album.title}</p>`;
+            albumsContainer?.appendChild(albumItem);
+
+            // Add eventListener on albumItem
+            albumItem.addEventListener('click',(e)=>{
+                displayComments(album.id)
+            })
+        })
+    }
+    displayAlbums()
+
+}catch(e){
+    throwError(e);
+}
